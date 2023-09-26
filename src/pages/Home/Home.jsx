64d53d16e65526f65c4904f77963f76c7c568bd9
Dextrodark <faizalmohamed5302002@gmail.com>
2023-09-26 16:11:59 +0530

    completed api, home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+    import React from 'react';
+    import "./home.css";
+
+    function Home() {
+    return (
+        <div>
+            Home
+        </div>
+    )
+    }
+
+    export default Home
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,19 +1,38 @@
-    import React, { useState } from 'react';
+    import React, { useState , useEffect } from 'react';
     import "./main.css";
     import SideMenu from '../../components/SideMenu/SideMenu';
     import Header from '../Header/Header';
+    import Home from '../Home/Home';
+
     function Main() {
         const[active,setActive] = useState(false);
+        const[games,setGames] = useState([]);
 
         const handleToggleActive =()=>{
             setActive(!active);
         };
+
+        const fetchData = ()=>{
+            fetch('http://localhost:3000/api/gamesData.json')
+            .then(res=>res.json())
+            .then(data=>{
+                setGames(data);
+            })
+            .catch(e=>console.log(e.message));
+        };
+
+        useEffect(()=>{
+            fetchData();
+        },[])
 
     return (
         <main>
             <SideMenu active={active}/>
             <div className={`banner ${active ? 'active' : undefined}`}>
                 <Header toggleActive={handleToggleActive}/>
+                <div className="container-fluid">
+                    <Home games={games} />
+                </div>
             </div>
         </main>
     )
