8a14a673b68bff46f5e7bc215bae20691d1d8c7b
Dextrodark <faizalmohamed5302002@gmail.com>
2023-09-27 12:25:49 +0530

    Section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,6 +9,16 @@ import GameSwiper from '../../components/GameSwiper/GameSwiper';
                 <div className="row">
                     <GameSwiper games={games} />
                 </div>
+                <div className="row">
+                    <div className="col-lg-6">
+                        <h2 className="sectionTitle">Games on promotion</h2>
+                    </div>
+                    <div className="col-lg-6 d-flex justify-content-end align-items-center">
+                        <a href="#" className='viewMore'>
+                            View More Games <i className="bi bi-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
             </div>
         </section>
     )
